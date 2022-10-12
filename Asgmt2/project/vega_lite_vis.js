57594b84c87a67086de9fba7a0f5150a5a64e62b
imswagk727 <imswagk727@gmail.com>
2022-10-13 00:26:30 +1100

    visitor map done

--- Asgmt2/project/vega_lite_vis.js
+++ Asgmt2/project/vega_lite_vis.js
@@ -3,6 +3,10 @@ vegaEmbed("#monthly_temperature_bar_chart", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "test.vg.json";
-vegaEmbed("#test", vg_2).then(function (result) {
+var vg_2 = "js/visitor_map.vg.json";
+vegaEmbed("#visitor_map", vg_2).then(function (result) {
+}).catch(console.error);
+
+var vg_3 = "test.vg.json";
+vegaEmbed("#test", vg_3).then(function (result) {
 }).catch(console.error);
