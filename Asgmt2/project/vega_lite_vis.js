0ceb9321b55b03caa3b1f9ff8358082b88f715bb
imswagk727 <imswagk727@gmail.com>
2022-10-13 01:47:57 +1100

    added bubble chart and visitor bar chart

--- Asgmt2/project/vega_lite_vis.js
+++ Asgmt2/project/vega_lite_vis.js
@@ -7,6 +7,14 @@ var vg_2 = "js/visitor_map.vg.json";
 vegaEmbed("#visitor_map", vg_2).then(function (result) {
 }).catch(console.error);
 
-var vg_3 = "test.vg.json";
-vegaEmbed("#test", vg_3).then(function (result) {
+var vg_3 = "js/travel_types_bubble_chart.vg.json";
+vegaEmbed("#travel_types_bubble_chart", vg_3).then(function (result) {
+}).catch(console.error);
+
+var vg_4 = "js/visitor_nationality.vg.json";
+vegaEmbed("#visitor_nationality", vg_4).then(function (result) {
+}).catch(console.error);
+
+var vg_10 = "test.vg.json";
+vegaEmbed("#test", vg_10).then(function (result) {
 }).catch(console.error);
