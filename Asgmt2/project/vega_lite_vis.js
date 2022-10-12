5ce148800d263cb4dfbd2558dfc6e75f201d5438
imswagk727 <imswagk727@gmail.com>
2022-10-12 19:07:51 +1100

    add visitor data

--- Asgmt2/project/vega_lite_vis.js
+++ Asgmt2/project/vega_lite_vis.js
@@ -3,6 +3,6 @@ vegaEmbed("#monthly_temperature_bar_chart", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "map.vg.json";
-vegaEmbed("#map", vg_2).then(function (result) {
+var vg_2 = "test.vg.json";
+vegaEmbed("#test", vg_2).then(function (result) {
 }).catch(console.error);
