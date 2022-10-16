ab2b335735c05f317b74251bb8a180c3edd31d65
imswagk727 <imswagk727@gmail.com>
2022-10-17 01:45:25 +1100

    update html and css

--- Asgmt2/project/vega_lite_vis.js
+++ Asgmt2/project/vega_lite_vis.js
@@ -1,20 +1,25 @@
-var vg_1 = "js/monthly_temperature_bar_chart.vg.json";
-vegaEmbed("#monthly_temperature_bar_chart", vg_1).then(function (result) {
+var temp_connected_dot = "js/monthly_temperature_bar_chart.vg.json";
+vegaEmbed("#monthly_temperature_bar_chart", temp_connected_dot).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "js/visitor_map.vg.json";
-vegaEmbed("#visitor_map", vg_2).then(function (result) {
+var visitor_map = "js/visitor_map.vg.json";
+vegaEmbed("#visitor_map", visitor_map).then(function (result) {
 }).catch(console.error);
 
-var vg_3 = "js/travel_types_bubble_chart.vg.json";
-vegaEmbed("#travel_types_bubble_chart", vg_3).then(function (result) {
+var travel_types_bubble = "js/travel_types_bubble_chart.vg.json";
+vegaEmbed("#travel_types_bubble_chart", travel_types_bubble).then(function (result) {
 }).catch(console.error);
 
-var vg_4 = "js/visitor_nationality.vg.json";
-vegaEmbed("#visitor_nationality", vg_4).then(function (result) {
+var nationality_bar = "js/visitor_nationality.vg.json";
+vegaEmbed("#visitor_nationality", nationality_bar).then(function (result) {
 }).catch(console.error);
 
-var vg_10 = "test.vg.json";
-vegaEmbed("#test", vg_10).then(function (result) {
+
+var visitor_coming_bar = "js/visitor_coming_bar_chart.vg.json";
+vegaEmbed("#visitor_coming_bar_chart", visitor_coming_bar).then(function (result) {
 }).catch(console.error);
+
+// var vg_10 = "test.vg.json";
+// vegaEmbed("#test", vg_10).then(function (result) {
+// }).catch(console.error);
